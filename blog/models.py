a76dcb21b176ce40b3515fbfac1283e77a7d1147
user <murashkin_d@iuca.kg>
2023-09-11 16:28:18 +0600

    first commit

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length = 300)
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now=True)
+
+    def __str__ (self) -> str:
+        return self.title
